088b1fdb31a94ca506947e01adc67ef9dc41e1e3
Heber <116190313+hpagon@users.noreply.github.com>
2024-05-07 18:25:43 -0700

    Added movie and tv series insert functions

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ function Media(title) {
     this.title = title;
     this.card = document.createElement("div");
     this.header = document.createElement("h3");
+    this.person = document.createElement("p");
     this.info = document.createElement("div");
 }
 
@@ -41,24 +42,70 @@ Book.prototype.insert = function () {
     this.card.setAttribute("id", myLibrary.length - 1)
     //create elements
     let image = document.createElement("img");
-    let author = document.createElement("p");
     let pages = document.createElement("p");
     let read = document.createElement("p");
     //add content
     this.header.innerHTML = this.title;
-    author.innerHTML = `Author: ${this.author}`;
+    this.person.innerHTML = `Author: ${this.author}`;
     pages.innerHTML = `Number of Pages: ${this.pages}`;
     read.innerHTML = this.read ? `Read`:`Not Read`;
     //append elements
     this.card.appendChild(this.header);
     this.card.appendChild(image);
-    this.info.appendChild(author);
+    this.info.appendChild(this.person);
     this.info.appendChild(pages);
     this.info.appendChild(read);
     this.card.appendChild(this.info);
     libraryDiv.appendChild(this.card);
 }
 
+Movie.prototype.insert = function () {
+    //add class info
+    this.card.classList.add("card");
+    this.info.classList.add("info");
+    this.card.setAttribute("id", myLibrary.length - 1)
+    //create elements
+    let image = document.createElement("img");
+    let length = document.createElement("p");
+    let seen = document.createElement("p");
+    //add content
+    this.header.innerHTML = this.title;
+    this.person.innerHTML = `Director: ${this.director}`;
+    length.innerHTML = `Length (minutes): ${this.length}`;
+    seen.innerHTML = this.seen ? `Seen`:`Not Seen`;
+    //append elements
+    this.card.appendChild(this.header);
+    this.card.appendChild(image);
+    this.info.appendChild(this.person);
+    this.info.appendChild(length);
+    this.info.appendChild(seen);
+    this.card.appendChild(this.info);
+    libraryDiv.appendChild(this.card);
+}
+
+TVSeries.prototype.insert = function () {
+    //add class info
+    this.card.classList.add("card");
+    this.info.classList.add("info");
+    this.card.setAttribute("id", myLibrary.length - 1)
+    //create elements
+    let image = document.createElement("img");
+    let episodes = document.createElement("p");
+    let seen = document.createElement("p");
+    //add content
+    this.header.innerHTML = this.title;
+    this.person.innerHTML = `Director: ${this.director}`;
+    episodes.innerHTML = `Number of Episodes: ${this.episodes}`;
+    seen.innerHTML = this.seen ? `Seen`:`Not Seen`;
+    //append elements
+    this.card.appendChild(this.header);
+    this.card.appendChild(image);
+    this.info.appendChild(this.person);
+    this.info.appendChild(episodes);
+    this.info.appendChild(seen);
+    this.card.appendChild(this.info);
+    libraryDiv.appendChild(this.card);
+}
 // Link prototypes
 Object.setPrototypeOf(Book.prototype, Media.prototype);
 Object.setPrototypeOf(Movie.prototype, Media.prototype);
@@ -85,6 +132,9 @@ function addItemToLibrary(type, author, title, pages, read) {
 addItemToLibrary("Book", "George R.R. Martin", "A Song of Ice and Fire", 786, false);
 addItemToLibrary("Book", "Ray Bradbury", "Fahrenheit 451", 212, true);
 addItemToLibrary("Book", "Rick Riordan", "Percy Jackson", 230, true);
+
+addItemToLibrary("Movie", "Christopher Nolan", "Inception", 145, true);
+addItemToLibrary("TVSeries", "Vince Gilligan", "Breaking Bad", 55, true);
 
 myLibrary.forEach(book => {
     console.log(book);

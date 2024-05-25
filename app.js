77067b9fc6a7ac911a3d19f0a024cf998e183e70
Heber <116190313+hpagon@users.noreply.github.com>
2024-05-25 12:58:00 -0700

    Refactored constructor styled code into classes

--- app.js
+++ app.js
@@ -15,197 +15,200 @@ const formClose = document.querySelector("dialog img");
 const emptyMessage = document.querySelector("#empty-message");
 
 //Media Object (meant to be parent object)
-function Media(title) {
-  this.title = title;
-  this.card = document.createElement("div");
-  this.header = document.createElement("h3");
-  this.person = document.createElement("p");
-  this.info = document.createElement("div");
-  this.toggle = document.createElement("button");
-  this.delete = document.createElement("img");
-  this.buttons = document.createElement("div");
-  this.personBolded = document.createElement("b");
-  this.personDiv = document.createElement("div");
-  this.lengthBolded = document.createElement("b");
-  this.lengthDiv = document.createElement("div");
-  this.typeBolded = document.createElement("b");
-  this.typeDiv = document.createElement("div");
-  this.typeP = document.createElement("p");
-  //delete function
-  this.delete.addEventListener("click", () => {
-    delete myLibrary[this.card.id]; //delete from library array
-    this.card.remove(); //remove from dom
-    availableIndices.push(parseInt(this.card.id)); //add index to available index stack
-    isEmpty();
-  });
+class Media {
+  //class fields
+  card = document.createElement("div");
+  header = document.createElement("h3");
+  person = document.createElement("p");
+  info = document.createElement("div");
+  toggle = document.createElement("button");
+  delete = document.createElement("img");
+  buttons = document.createElement("div");
+  personBolded = document.createElement("b");
+  personDiv = document.createElement("div");
+  lengthBolded = document.createElement("b");
+  lengthDiv = document.createElement("div");
+  typeBolded = document.createElement("b");
+  typeDiv = document.createElement("div");
+  typeP = document.createElement("p");
+  constructor(title) {
+    this.title = title;
+    this.delete.addEventListener("click", () => {
+      delete myLibrary[this.card.id]; //delete from library array
+      this.card.remove(); //remove from dom
+      availableIndices.push(parseInt(this.card.id)); //add index to available index stack
+      isEmpty();
+    });
+  }
 }
 
 //Book object (inherits from media)
-function Book(author, title, pages, read) {
-  Media.call(this, title);
-  this.type = "Book";
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.toggle.addEventListener("click", () => {
-    this.read = this.read ? false : true;
-    this.toggle.innerHTML = this.read ? "Read" : "Not Read";
+class Book extends Media {
+  constructor(author, title, pages, read) {
+    super(title);
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.type = "Book";
+    this.toggle.addEventListener("click", () => {
+      this.read = this.read ? false : true;
+      this.toggle.innerHTML = this.read ? "Read" : "Not Read";
+      this.toggle.style.backgroundColor = this.read ? "green" : "#6C757D";
+    });
+  }
+  //inserts card for new book
+  insert(index) {
+    //add class info
+    this.card.classList.add("card");
+    this.info.classList.add("info");
+    this.card.setAttribute("id", index);
+    this.buttons.classList.add("info-buttons");
+    //create elements
+    let image = document.createElement("img");
+    let pages = document.createElement("p");
+    //add content
+    this.header.innerHTML = this.title;
+    this.personBolded.innerHTML = "Author: ";
+    this.person.innerHTML = ` ${this.author}`;
+    this.lengthBolded.innerHTML = "Number of Pages: ";
+    pages.innerHTML = ` ${this.pages}`;
+    this.toggle.innerHTML = this.read ? `Read` : `Not Read`;
     this.toggle.style.backgroundColor = this.read ? "green" : "#6C757D";
-  });
+    image.src = "images/green.jpg";
+    this.delete.src = "images/close.svg";
+    this.typeBolded.innerHTML = "Type: ";
+    this.typeP.innerHTML = this.type;
+    //append elements
+    this.card.appendChild(this.header);
+    this.card.appendChild(image);
+    this.typeDiv.appendChild(this.typeBolded);
+    this.typeDiv.appendChild(this.typeP);
+    this.info.appendChild(this.typeDiv);
+    this.personDiv.appendChild(this.personBolded);
+    this.personDiv.appendChild(this.person);
+    this.info.appendChild(this.personDiv);
+    this.lengthDiv.appendChild(this.lengthBolded);
+    this.lengthDiv.appendChild(pages);
+    this.info.appendChild(this.lengthDiv);
+    this.buttons.appendChild(this.toggle);
+    this.buttons.appendChild(this.delete);
+    this.info.appendChild(this.buttons);
+    this.card.appendChild(this.info);
+    libraryDiv.prepend(this.card);
+  }
 }
 
 //Movie object (inherits from media)
-function Movie(director, title, length, seen) {
-  Media.call(this, title);
-  this.type = "Movie";
-  this.director = director;
-  this.length = length;
-  this.seen = seen;
-  this.toggle.addEventListener("click", () => {
-    this.seen = this.seen ? false : true;
-    this.toggle.innerHTML = this.seen ? "Seen" : "Not Seen";
+class Movie extends Media {
+  constructor(director, title, length, seen) {
+    super(title);
+    this.type = "Movie";
+    this.director = director;
+    this.length = length;
+    this.seen = seen;
+    this.toggle.addEventListener("click", () => {
+      this.seen = this.seen ? false : true;
+      this.toggle.innerHTML = this.seen ? "Seen" : "Not Seen";
+      this.toggle.style.backgroundColor = this.seen ? "green" : "#6C757D";
+    });
+  }
+  //inserts card for new movie
+  insert(index) {
+    //add class info
+    this.card.classList.add("card");
+    this.info.classList.add("info");
+    this.card.setAttribute("id", index);
+    this.buttons.classList.add("info-buttons");
+    //create elements
+    let image = document.createElement("img");
+    let length = document.createElement("p");
+    //add content
+    this.header.innerHTML = this.title;
+    this.personBolded.innerHTML = "Director: ";
+    this.person.innerHTML = ` ${this.director}`;
+    this.lengthBolded.innerHTML = "Length(minutes): ";
+    length.innerHTML = ` ${this.length}`;
+    this.toggle.innerHTML = this.seen ? `Seen` : `Not Seen`;
     this.toggle.style.backgroundColor = this.seen ? "green" : "#6C757D";
-  });
+    image.src = "images/red.jpg";
+    this.delete.src = "images/close.svg";
+    this.typeBolded.innerHTML = this.type + ": ";
+    this.typeP.innerHTML = this.type;
+    //append elements
+    this.card.appendChild(this.header);
+    this.card.appendChild(image);
+    this.typeDiv.appendChild(this.typeBolded);
+    this.typeDiv.appendChild(this.typeP);
+    this.info.appendChild(this.typeDiv);
+    this.personDiv.appendChild(this.personBolded);
+    this.personDiv.appendChild(this.person);
+    this.info.appendChild(this.personDiv);
+    this.lengthDiv.appendChild(this.lengthBolded);
+    this.lengthDiv.appendChild(length);
+    this.info.appendChild(this.lengthDiv);
+    this.buttons.appendChild(this.toggle);
+    this.buttons.appendChild(this.delete);
+    this.info.appendChild(this.buttons);
+    this.card.appendChild(this.info);
+    libraryDiv.prepend(this.card);
+  }
 }
 
 //TVSeries object (inherits from media)
-function TVSeries(director, title, episodes, seen) {
-  Media.call(this, title);
-  this.type = "TV Series";
-  this.director = director;
-  this.episodes = episodes;
-  this.seen = seen;
-  this.toggle.addEventListener("click", () => {
-    this.seen = this.seen ? false : true;
-    this.toggle.innerHTML = this.seen ? "Seen" : "Not Seen";
+class TVSeries extends Media {
+  constructor(director, title, episodes, seen) {
+    super(title);
+    this.type = "TV Series";
+    this.director = director;
+    this.episodes = episodes;
+    this.seen = seen;
+    this.toggle.addEventListener("click", () => {
+      this.seen = this.seen ? false : true;
+      this.toggle.innerHTML = this.seen ? "Seen" : "Not Seen";
+      this.toggle.style.backgroundColor = this.seen ? "green" : "#6C757D";
+    });
+  }
+  //inserts card for new tv series
+  insert(index) {
+    //add class info
+    this.card.classList.add("card");
+    this.info.classList.add("info");
+    this.card.setAttribute("id", index);
+    this.buttons.classList.add("info-buttons");
+    //create elements
+    let image = document.createElement("img");
+    let episodes = document.createElement("p");
+    //add content
+    this.header.innerHTML = this.title;
+    this.personBolded.innerHTML = "Creator: ";
+    this.person.innerHTML = ` ${this.director}`;
+    this.lengthBolded.innerHTML = "Episodes: ";
+    episodes.innerHTML = ` ${this.episodes}`;
+    this.toggle.innerHTML = this.seen ? `Seen` : `Not Seen`;
     this.toggle.style.backgroundColor = this.seen ? "green" : "#6C757D";
-  });
+    image.src = "images/pastel.jpg";
+    this.delete.src = "images/close.svg";
+    this.typeBolded.innerHTML = this.type + ": ";
+    this.typeP.innerHTML = this.type;
+    //append elements
+    this.card.appendChild(this.header);
+    this.card.appendChild(image);
+    this.typeDiv.appendChild(this.typeBolded);
+    this.typeDiv.appendChild(this.typeP);
+    this.info.appendChild(this.typeDiv);
+    this.personDiv.appendChild(this.personBolded);
+    this.personDiv.appendChild(this.person);
+    this.info.appendChild(this.personDiv);
+    this.lengthDiv.appendChild(this.lengthBolded);
+    this.lengthDiv.appendChild(episodes);
+    this.info.appendChild(this.lengthDiv);
+    this.buttons.appendChild(this.toggle);
+    this.buttons.appendChild(this.delete);
+    this.info.appendChild(this.buttons);
+    this.card.appendChild(this.info);
+    libraryDiv.prepend(this.card);
+  }
 }
-
-//inserts card for new book
-Book.prototype.insert = function (index) {
-  //add class info
-  this.card.classList.add("card");
-  this.info.classList.add("info");
-  this.card.setAttribute("id", index);
-  this.buttons.classList.add("info-buttons");
-  //create elements
-  let image = document.createElement("img");
-  let pages = document.createElement("p");
-  //add content
-  this.header.innerHTML = this.title;
-  this.personBolded.innerHTML = "Author: ";
-  this.person.innerHTML = ` ${this.author}`;
-  this.lengthBolded.innerHTML = "Number of Pages: ";
-  pages.innerHTML = ` ${this.pages}`;
-  this.toggle.innerHTML = this.read ? `Read` : `Not Read`;
-  this.toggle.style.backgroundColor = this.read ? "green" : "#6C757D";
-  image.src = "images/green.jpg";
-  this.delete.src = "images/close.svg";
-  this.typeBolded.innerHTML = "Type: ";
-  this.typeP.innerHTML = this.type;
-  //append elements
-  this.card.appendChild(this.header);
-  this.card.appendChild(image);
-  this.typeDiv.appendChild(this.typeBolded);
-  this.typeDiv.appendChild(this.typeP);
-  this.info.appendChild(this.typeDiv);
-  this.personDiv.appendChild(this.personBolded);
-  this.personDiv.appendChild(this.person);
-  this.info.appendChild(this.personDiv);
-  this.lengthDiv.appendChild(this.lengthBolded);
-  this.lengthDiv.appendChild(pages);
-  this.info.appendChild(this.lengthDiv);
-  this.buttons.appendChild(this.toggle);
-  this.buttons.appendChild(this.delete);
-  this.info.appendChild(this.buttons);
-  this.card.appendChild(this.info);
-  libraryDiv.prepend(this.card);
-};
-//inserts card for new movie
-Movie.prototype.insert = function (index) {
-  //add class info
-  this.card.classList.add("card");
-  this.info.classList.add("info");
-  this.card.setAttribute("id", index);
-  this.buttons.classList.add("info-buttons");
-  //create elements
-  let image = document.createElement("img");
-  let length = document.createElement("p");
-  //add content
-  this.header.innerHTML = this.title;
-  this.personBolded.innerHTML = "Director: ";
-  this.person.innerHTML = ` ${this.director}`;
-  this.lengthBolded.innerHTML = "Length(minutes): ";
-  length.innerHTML = ` ${this.length}`;
-  this.toggle.innerHTML = this.seen ? `Seen` : `Not Seen`;
-  this.toggle.style.backgroundColor = this.seen ? "green" : "#6C757D";
-  image.src = "images/red.jpg";
-  this.delete.src = "images/close.svg";
-  this.typeBolded.innerHTML = this.type + ": ";
-  this.typeP.innerHTML = this.type;
-  //append elements
-  this.card.appendChild(this.header);
-  this.card.appendChild(image);
-  this.typeDiv.appendChild(this.typeBolded);
-  this.typeDiv.appendChild(this.typeP);
-  this.info.appendChild(this.typeDiv);
-  this.personDiv.appendChild(this.personBolded);
-  this.personDiv.appendChild(this.person);
-  this.info.appendChild(this.personDiv);
-  this.lengthDiv.appendChild(this.lengthBolded);
-  this.lengthDiv.appendChild(length);
-  this.info.appendChild(this.lengthDiv);
-  this.buttons.appendChild(this.toggle);
-  this.buttons.appendChild(this.delete);
-  this.info.appendChild(this.buttons);
-  this.card.appendChild(this.info);
-  libraryDiv.prepend(this.card);
-};
-//inserts card for new tv series
-TVSeries.prototype.insert = function (index) {
-  //add class info
-  this.card.classList.add("card");
-  this.info.classList.add("info");
-  this.card.setAttribute("id", index);
-  this.buttons.classList.add("info-buttons");
-  //create elements
-  let image = document.createElement("img");
-  let episodes = document.createElement("p");
-  //add content
-  this.header.innerHTML = this.title;
-  this.personBolded.innerHTML = "Creator: ";
-  this.person.innerHTML = ` ${this.director}`;
-  this.lengthBolded.innerHTML = "Episodes: ";
-  episodes.innerHTML = ` ${this.episodes}`;
-  this.toggle.innerHTML = this.seen ? `Seen` : `Not Seen`;
-  this.toggle.style.backgroundColor = this.seen ? "green" : "#6C757D";
-  image.src = "images/pastel.jpg";
-  this.delete.src = "images/close.svg";
-  this.typeBolded.innerHTML = this.type + ": ";
-  this.typeP.innerHTML = this.type;
-  //append elements
-  this.card.appendChild(this.header);
-  this.card.appendChild(image);
-  this.typeDiv.appendChild(this.typeBolded);
-  this.typeDiv.appendChild(this.typeP);
-  this.info.appendChild(this.typeDiv);
-  this.personDiv.appendChild(this.personBolded);
-  this.personDiv.appendChild(this.person);
-  this.info.appendChild(this.personDiv);
-  this.lengthDiv.appendChild(this.lengthBolded);
-  this.lengthDiv.appendChild(episodes);
-  this.info.appendChild(this.lengthDiv);
-  this.buttons.appendChild(this.toggle);
-  this.buttons.appendChild(this.delete);
-  this.info.appendChild(this.buttons);
-  this.card.appendChild(this.info);
-  libraryDiv.prepend(this.card);
-};
-// Link prototypes
-Object.setPrototypeOf(Book.prototype, Media.prototype);
-Object.setPrototypeOf(Movie.prototype, Media.prototype);
-Object.setPrototypeOf(TVSeries.prototype, Media.prototype);
 
 //Adds item to library array
 function addItemToLibrary(type, author, title, pages, read) {
